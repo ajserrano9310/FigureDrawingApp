62118df5f51e61a0e069d2ab780bdb45ec1f8fad
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-05-30 20:26:28 -0600

    Day 15. Setting up some stuff for mongodb database

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+
+/*
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+*/
 const express = require('express');
 const path = require('path'); 
 const app = express(); 
@@ -12,6 +18,48 @@ app.get('/gesture', function(req, res){
 
 app.use(express.static('styles')); 
 app.use(express.static('scripts')); 
+
+const {MongoClient} = require('mongodb'); 
+
+/*
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParses: true
+}); 
+
+const db = mongoose.connection; 
+db.on('error', error => console.error(error)); 
+db.once('open', () => console.log('connected')); 
+*/
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function main(){
+    
+    const client = new MongoClient(uri, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }); 
+
+    try{
+        await client.connect(); 
+
+        await listDatabases(client); 
+    }
+    catch (e){
+        console.error(e);
+    }finally{
+        await client.close();
+    }
+}
+
+main().catch(console.error)
+
 
 const server = app.listen(8000, function () {
     let host = server.address().address
