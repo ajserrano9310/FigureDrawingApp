d577c032d4638e79b2b50f1d67388cd878901e74
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2022-05-31 19:08:08 -0600

    making progress with the back end

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,24 +1,22 @@
-
 // GLOBALS
 
 const minutes = 60;
 var timeInterval;
 
 var imagesFromDB = [];
-var images = []; 
+var images = [];
 var index = 0;
-const imageID = 'current';
+const imageID = "current";
 
-const image_input = document.querySelector('#image_input');
+const image_input = document.querySelector("#image_input");
 
-var uploadedImage = '';
-
+var uploadedImage = "";
 
 /**
  * MODAL CODE
  * */
 var modal = document.getElementById("myModal");
-var img = document.getElementById("img"); 
+var img = document.getElementById("img");
 
 var modalImg = document.getElementById("img01");
 
@@ -27,12 +25,11 @@ var captionText = document.getElementById("caption");
 /* CLOSE IMAGE VIEWER
  **/
 
-var closeBtn = document.getElementById('closeBTN');
+var closeBtn = document.getElementById("closeBTN");
 closeBtn.onclick = function () {
-    modal.style.display = 'none';
-    stopTimer(); 
-}
-
+  modal.style.display = "none";
+  stopTimer();
+};
 
 //First Commit?
 //Another commit for checking purposes
@@ -41,95 +38,86 @@ closeBtn.onclick = function () {
 //var timerDisplay = document.getElementById("timer");
 
 /*
- * SET ARRAY OF IMAGES 
+ * SET ARRAY OF IMAGES
  * **/
 
-if(image_input){
-    image_input.addEventListener("change", function (event) {
-        let files = event.target.files;
-    
-        for (let i = 0; i < files.length; i++) {
-            imagesFromDB.push(
-                URL.createObjectURL(files[i]));
-          
-        }
-    })
+if (image_input) {
+  image_input.addEventListener("change", function (event) {
+    let files = event.target.files;
+
+    for (let i = 0; i < files.length; i++) {
+      imagesFromDB.push(URL.createObjectURL(files[i]));
+    }
+  });
 }
-
-
 
 /***
  * ACTIVATE MODAL FOR VIEWING IMAGES
  * */
 function activateModal() {
-
-    if(modal){
-
-        setTimer(); 
-        modal.style.display = "block";
-        setImage(0); 
-    }
+  if (modal) {
+    setTimer();
+    modal.style.display = "block";
+    setImage(0);
+  }
 }
 
-
 function setImage(i) {
-    let urlString = 'url(' + imagesFromDB[i] + ')';
-    modal.style.backgroundImage = urlString; 
+  let urlString = "url(" + imagesFromDB[i] + ")";
+  modal.style.backgroundImage = urlString;
 }
 
 /**
  *  Start the timer for practice.
- * 
+ *
  * @param {any} duration sets the time of the timer
  * @param {any} display where the timer will be showing
  */
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+  var timer = duration,
+    minutes,
+    seconds;
 
-        timeInterval = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+  timeInterval = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+    display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-            index++;
-            resetImage(index);
-            console.log("reset image"); 
-            
-        }
-    }, 1000);
-
-   
+    if (--timer < 0) {
+      timer = duration;
+      index++;
+      resetImage(index);
+      console.log("reset image");
+    }
+  }, 1000);
 }
 
 /**
- * Stop the session timer 
+ * Stop the session timer
  * */
 function stopTimer() {
-    clearInterval(timeInterval);
-    alert("timer stopped");
+  clearInterval(timeInterval);
+  alert("timer stopped");
 }
 
 /**
  * Get the time selected by user and
- * start session timer. 
+ * start session timer.
  * */
 function setTimer() {
+  var time = getRadio();
+  if (typeof time == "undefined") {
+    alert("Please select a time");
+    return undefined;
+  }
 
-    var time = getRadio();
-    if (typeof time == 'undefined') {
-        alert("Please select a time");
-        return undefined; 
-    }
-
-    let fiveMinutes = minutes * time, display = document.querySelector('#timer-caption');
-    startTimer(fiveMinutes, display);
-
+  let fiveMinutes = minutes * time,
+    display = document.querySelector("#timer-caption");
+  startTimer(fiveMinutes, display);
 }
 
 /**
@@ -138,59 +126,57 @@ function setTimer() {
  * an error.
  * */
 function getRadio() {
-    var elements = document.getElementsByName('time');
-    var chosenTime;
+  var elements = document.getElementsByName("time");
+  var chosenTime;
 
-    for (i = 0; i < elements.length; i++) {
-        if (elements[i].checked) {
-            chosenTime = elements[i].value;
-        }
+  for (i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      chosenTime = elements[i].value;
     }
+  }
 
-    if (typeof chosenTime === 'undefined') {
-        return undefined;
-    }
-    else {
-        var time = parseInt(chosenTime);
-        return time;
-    }
+  if (typeof chosenTime === "undefined") {
+    return undefined;
+  } else {
+    var time = parseInt(chosenTime);
+    return time;
+  }
 }
 
 /**
- * Load the images from Database 
+ * Load the images from Database
  * */
 function fetchImages() {
+  alert("Baby steps");
+  var url = "/getImages";
+  
+  $.post(url)
+    .done(function(result) {
+        console.log('an iq 159');
+        //setImages(result); 
+    }).always(function(){
+        console.log('aways option')
+    })
+    .fail(function(){
+        console.log("this failed"); 
+    })
 
-    alert("fetching images");
-
-    imagesFromDB.push("https://i.ibb.co/VtQWxdx/the-killer-elite-3-by-mjranum-stock-d1fllr0-fullview.jpg"); 
-    imagesFromDB.push("https://i.ibb.co/HHB874n/14910.jpg"); 
-    imagesFromDB.push("https://i.ibb.co/8Y51HwM/this-will-hardly-hurt-2-by-mjranum-stock-d321gkz-fullview.jpg"); 
-
-    /*
-    $.ajax({
-        url : "/Home/FetchImages",
-        type: "POST",
-        success: function (data) {
-            setImages(data);
-        }
-    });
-    */
 }
 
 function setImages(images) {
-    imagesFromDB = images;
+  imagesFromDB = images;
+}
+
+function getImages(){
+    console.log(imagesFromDB); 
+    return imagesFromDB; 
 }
 
 function resetImage(index) {
+  if (index >= imagesFromDB.length) {
+    alert("Images are done");
+    return;
+  }
 
-    if (index >= imagesFromDB.length) {
-        alert("Images are done");
-        return; 
-    }
-
-    setImage(index);
-
+  setImage(index);
 }
-
-
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 
+
+
+var arrayOfUrls = []; 
+
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -11,104 +15,53 @@ app.get("/gesture", function (req, res) {
   res.sendFile(path.resolve("views/gestureView.html"));
 });
 
+app.post('/getImages', async function(req, res){
+  
+  console.log('Ajax call succeded'); 
+  var namesS = ['Tony soprano', 'Carmela Soprano', 'Meadow Soprano', 'AJ Soprano']; 
+
+  var array = getImagesFromDB(); 
+  console.log(array); 
+
+  //res.send(arrayOfUrls);
+
+
+  return 'something'; 
+})
 app.use(express.static("styles"));
 app.use(express.static("scripts"));
-
-/*
-const {MongoClient} = require('mongodb'); 
-
-
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParses: true
-}); 
-
-const db = mongoose.connection; 
-db.on('error', error => console.error(error)); 
-db.once('open', () => console.log('connected')); 
-
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-async function main(){
-    
-    const client = new MongoClient(uri, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true
-    }); 
-
-    try{
-        await client.connect(); 
-
-        await listDatabases(client); 
-    }
-    catch (e){
-        console.error(e);
-    }finally{
-        await client.close();
-    }
-}
-
-main().catch(console.error)
-*/
 
 var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/db-images";
 
-MongoClient.connect(url, function (err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("db-images");
-  dbo.createCollection("image-repo", function(err, res) {
-    if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
-/*
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("db-images");
-
-
-    //var myobj = { name: "Company Inc", address: "Highway 37" };
-
-    var imageURLS = [
-        {id: 1, gender: "female", url: "https://i.ibb.co/VtQWxdx/the-killer-elite-3-by-mjranum-stock-d1fllr0-fullview.jpg"}, 
-        {id: 2, gender: "female", url: "https://i.ibb.co/HHB874n/14910.jpg"},
-        {id: 3, gender: "female", url: "https://i.ibb.co/8Y51HwM/this-will-hardly-hurt-2-by-mjranum-stock-d321gkz-fullview.jpg"}
-    ]
-    
-    dbo.collection("image-repo").insertMany(imageURLS, function(err, res) {
-      if (err) throw err;
-      console.log(imageURLS + "documents inserted");
-      db.close();
-    });
-  });
-
-  MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("db-images");
-  dbo.collection("image-repo").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-  });
-});
-*/
 
 const server = app.listen(8000, function () {
   let host = server.address().address;
   let port = server.address().port;
   // Starting the Server at the port 3000
 });
+
+async function saveImagesLocally(result) {
+
+  arrayOfUrls = result; 
+  console.log(arrayOfUrls);
+
+}
+
+
+function getImagesFromDB(){
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("db-images");
+    dbo
+      .collection("repo")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        console.log("during client call:" +  result.length);  
+        saveImagesLocally(result); 
+        db.close();
+        return result; 
+      });
+  });
+}
